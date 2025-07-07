c7720bb059abc6dc3274ef7d688caf86d25fcc3c
Carlos Quintanilha <cquinta@gmail.com>
2025-07-07 12:08:26 -0300

    parameter-store

--- secrets_manager.tf
+++ secrets_manager.tf
@@ -1,14 +1,16 @@
-resource "aws_secretsmanager_secret" "teste" {
-  name = "chip-teste"
+resource "aws_secretsmanager_secret" "teste-secret" {
+  name = "chip-teste1"
+  recovery_window_in_days = 0
 }
 
-resource "aws_secretsmanager_secret_version" "teste" {
-  secret_id     = aws_secretsmanager_secret.teste.id
+resource "aws_secretsmanager_secret_version" "teste-secret" {
+  secret_id     = aws_secretsmanager_secret.teste-secret.id
   secret_string = "BAR"
 }
 
 resource "aws_secretsmanager_secret" "teste_json" {
-  name = "chip-teste-json"
+  name = "chip-teste-json1"
+  recovery_window_in_days = 0
 }
 
 resource "aws_secretsmanager_secret_version" "teste_json" {
